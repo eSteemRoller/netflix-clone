8cbf7ad0928af6a4053a017f3678e3893e0d8a65
Jason <jasondoroughonfb@live.com>
2025-10-09 22:37:47 -0700

    Fourth (Home page, Part 4) commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 
 import React from 'react';
 import './Footer.css';
+import facebook_icon from '../../assets/facebook_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import twitter_icon from '../../assets/twitter_icon.png';
+import youtube_icon from '../../assets/youtube_icon.png';
 
 
 export default function Footer() { 
@@ -8,7 +12,27 @@ export default function Footer() {
 
   return (
     <div className='footer'>
-
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="facebook icon" />
+        <img src={instagram_icon} alt="instagram icon" />
+        <img src={twitter_icon} alt="twitter icon" />
+        <img src={youtube_icon} alt="youtube icon" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Support</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>© 2025 Netflix?, Inc.</p>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import hero_title from '../../assets/hero_title.png';
 import play_icon from '../../assets/play_icon.png';
 import info_icon from '../../assets/info_icon.png';
 import TitleCards from '../../components/TitleCards/TitleCards';
+import Footer from '../../components/Footer/Footer';
 
 
 export default function Home() { 
@@ -42,8 +43,9 @@ export default function Home() {
         <TitleCards title={'Blockbuster Movies'} />
         <TitleCards title={'Only On Netflix'} />
         <TitleCards title={'Coming Soon'} />
-        <TitleCards title={'My List'} />
+        <TitleCards title={'Recommended For You'} />
       </div>
+      <Footer />
     </div>
   )
 }
