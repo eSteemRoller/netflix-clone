42bfe6f00d241232868fccbf75b7c27ab2105b0f
Jason <jasondoroughonfb@live.com>
2025-10-12 22:56:22 -0700

    Seventh (Title cards, Part 2; API setup, Part 1) commit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/cards_data';
 
@@ -7,7 +7,17 @@ import cards_data from '../../assets/cards/cards_data';
 
 export default function TitleCards({title, category}) { 
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTE0MmExZmQzZjNlNGZmOWI0OGIzNmNkNzQwYzdjYyIsIm5iZiI6MTc2MDMzMTM5My40OTcsInN1YiI6IjY4ZWM4NjgxZWI4YjIwNzRjMGYxMTk5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-WQrlqznYUmxqytqsYvqdR3gMEy0kFrP9WG4Ayl7Lrg'
+    }
+  };
+
 
   const handleWheel = (event) => { 
     event.preventDefault();
@@ -15,6 +25,11 @@ export default function TitleCards({title, category}) {
   }
 
   useEffect(() => { 
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
   },[])
 
@@ -23,10 +38,10 @@ export default function TitleCards({title, category}) {
     <div className='title-cards'>
       <h2>{title?title:'New and Most Popular'}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => { 
+        {apiData.map((card, index) => { 
           return <div className="card" key={index}>
-            <img src={card.image} alt="movie poster image" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="movie poster image" />
+            <p>{card.original_title}</p>
           </div>
         } )}
       </div>
